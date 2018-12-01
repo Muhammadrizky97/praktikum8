70d1fc0adec275023d6359a0d2bcc59cf8158643
unknown <you@example.com>
2018-12-01 15:19:08 +0700

    mencari kasus lain dengan cara rekrusif dan iteratif

---
+++ latihan3/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+int main () {
+cout<<iostream& operator>>(istream& in,Fibo& a;
+cout << Deret Nilai;
+in>>a.n;
+return in;
+}
+int main(){
+Fibo run;
+cin >> run;
+cout << run;
+getch();
+}
